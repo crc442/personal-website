8a975efbfb9147d9d0727fd4f5ace86ea86ff038
crc442 <EequalsMCsquare91@gmail.com>
2015-03-02 01:01:09 -0500

    more clean up

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,10 @@
     var el = document.querySelector('#i-am');
     if (!el) return;
     var initialText = el.textContent;
-    var pause = 800
     var opts = {
         speed: 40,
         loop: false,
+        pauseDelay: 800,
         postfix: ''
     };
 
@@ -14,16 +14,16 @@
 
     typist
         .pause(2400).delete(initialText.length - 2)
-        .type('hero').pause(pause).delete(4)
-        .type('nerd').pause(pause).delete(6)
-        .type('sexy').pause(pause).delete(4)
-        .type('nodejs').pause(pause).delete(6)
-        .type('a pokemon master').pause(pause).delete(16)
-        .type('javascript').pause(pause).delete(10)
-        .type('a geek').pause(pause).delete(4)
-        .type('Web Developer').pause(pause).delete(14)
-        .type('n amateur Drummer').pause(pause).delete(18)
-        .type('that guy').pause(pause).delete(8)
+        .type('hero').pause().delete(4)
+        .type('nerd').pause().delete(6)
+        .type('sexy').pause().delete(4)
+        .type('nodejs').pause().delete(6)
+        .type('a pokemon master').pause().delete(16)
+        .type('javascript').pause().delete(10)
+        .type('a geek').pause().delete(4)
+        .type('Web Developer').pause().delete(14)
+        .type('n amateur Drummer').pause().delete(18)
+        .type('that guy').pause().delete(8)
         .type('Chirag.').pause(1200)
         .call(function() {
             el.className += " disabled"
