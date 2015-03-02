c37aa3ee6dc2d23cfd677f5edf2ff34d3c684e29
crc442 <EequalsMCsquare91@gmail.com>
2015-03-02 01:37:33 -0500

    DONE!

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@
     var opts = {
         speed: 40,
         loop: false,
-        pauseDelay: 800,
+        pauseDelay: 900,
         postfix: ''
     };
 
@@ -14,14 +14,14 @@
 
     typist
         .pause(2400).delete(initialText.length)
-        .type('a hero').pause().delete(4)
-        .type('nerd').pause().delete(6)
+        .type('a nerd').pause().delete(6)
         .type('sexy').pause().delete(4)
         .type('nodejs').pause().delete(6)
         .type('a pokemon master').pause().delete(16)
         .type('javascript').pause().delete(10)
+        .type('the karate kid').pause().delete(14)
         .type('a geek').pause().delete(4)
-        .type('a hacker').pause().delete(8)
+        .type('hacker').pause().delete(8)
         .type('Web Developer').pause().delete(14)
         .type('n amateur drummer').pause().delete(18)
         .type('a Software Engineer').pause().delete(19)
